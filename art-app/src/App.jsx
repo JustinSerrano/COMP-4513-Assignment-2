96f73c09c1cc5f7de62b40d22a5837df1bd0b52e
Justin Serrano <60021371+JustinSerrano@users.noreply.github.com>
2025-04-02 19:27:49 -0600

    First Deployment
    - art-app (React-Vite)
    - Tailwindcss/vite

---
+++ art-app/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+      <div className="text-3xl font-bold underline text-blue-500">
+      Hello Tailwind CSS!
+    </div>
+  );
+}
+
+export default App
