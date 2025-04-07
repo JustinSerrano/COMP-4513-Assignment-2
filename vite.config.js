4de303bb50849d1ce9d167fbb227a09b8b91aa17
Justin Serrano <60021371+JustinSerrano@users.noreply.github.com>
2025-04-07 03:45:09 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,26 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor';
+            }
+            if (id.includes('leaflet')) {
+              return 'leaflet-vendor';
+            }
+            if (id.includes('supabase')) {
+              return 'supabase-vendor';
+            }
+            return 'vendor';
+          }
+        }
       }
-    }
-  },
-  server: {
-    historyApiFallback: true
+    },
+    chunkSizeWarningLimit: 1000 // Increase the chunk size limit if needed
   }
-})
+});
